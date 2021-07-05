5ea0f71376415146bc9e06a3341b220043900dde
Katherine Blasman <b.katherine@hotmail.com>
2021-07-05 11:54:05 -0400

    figuring out banner

---
+++ src/components/Banner.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Banner = () => {
+    return (
+
+        <div>
+            <header class="banner">
+            
+            </header>
+        </div>
+
+    )
+}
+
+export default Banner
