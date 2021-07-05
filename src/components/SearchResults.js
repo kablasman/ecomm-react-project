bef68229cf8f3496d103256adb83cd161abeb944
Katherine Blasman <b.katherine@hotmail.com>
2021-07-05 11:48:38 -0400

    pagination

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -3,14 +3,17 @@ import ProductArray from 'components/ProductArray'
 
 const SearchResults = ({result}) => {
 
-    const theProducts = result.map((product) => <ProductArray key={product.bookTitle} data={product} />)
+    const theProducts = result.slice(0,20).map((product) => <ProductArray key={product.bookTitle} data={product} />)
 
     return (
 
         <div className="grid-container">
         {theProducts}
+
+        <div id="numStudents">
+            {theProducts.length} {(theProducts.length === 1) ? `product` : `products`} of {result.length}
         </div>
-        
+        </div>
 
     )
 }
