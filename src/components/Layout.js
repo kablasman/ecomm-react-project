bfa325e36ebb26a6afde7fb131d9a066e4c1d2b2
Katherine Blasman <b.katherine@hotmail.com>
2021-07-09 16:51:13 -0400

    adding and styling categories in layout

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -8,9 +8,32 @@ const Layout = ({children}) => {
         <div className="layout">
             <Header/>
             
-           
-
             <Banner />  
+            <form class="filters">
+                <h1>Shop by Category</h1>
+                <hr />
+                <div class="filter-options">
+                <fieldset>
+                    <legend className="fiction"><h2>Fiction</h2></legend>
+                </fieldset>
+                <fieldset>
+                    <legend className="non-fiction"><h2>Non-fiction</h2></legend>
+                </fieldset>
+                <fieldset>
+                    <legend className="wellness"><h2>Health/Wellness</h2></legend>
+                </fieldset>
+                <fieldset>
+                    <legend className="teens"><h2>Teens/Pre-teens</h2></legend>
+                </fieldset>
+                <fieldset>
+                    <legend className="kids"><h2>Kids 6-12</h2></legend>
+                </fieldset>
+                <fieldset>
+                    <legend className="popularity"><h2>Popularity</h2></legend>
+                </fieldset>
+                </div>
+                <hr />
+            </form>
             <main className="products">
             <section class="results">
             {children}
