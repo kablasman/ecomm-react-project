346617a4fa4202e79b9694a900935ba4a4dfaa10
Katherine Blasman <b.katherine@hotmail.com>
2021-08-05 17:38:20 -0400

    removing clutter

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,7 +1,14 @@
-import React from 'react'
+import React, {useState} from 'react'
 import ProductArray from 'components/ProductArray'
 
 const SearchResults = ({result}) => {
+
+    // const pageSize = 20;
+    // const [currentPage, setCurrentPage] = useState(1);
+    // const totalSize = 20;
+
+    // const productStart = (currentPage - 1) * pageSize
+    // const productEnd = productStart + pageSize
 
     const theProducts = result.slice(0,20).map((product) => <ProductArray key={product.bookTitle} data={product} />)
 
