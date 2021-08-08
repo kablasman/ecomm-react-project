577f73c7885b9f86dfd6b086bbdcebd553020bff
Katherine Blasman <b.katherine@hotmail.com>
2021-08-08 18:42:55 -0400

    adding product from single view to cart page

--- src/components/OneProduct.js
+++ src/components/OneProduct.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import ProductContext from 'contexts/oneproduct'
 import Image from 'components/Image'
 import AuthorImg from 'img/unsplash_photoshoppedimg.png'
 
@@ -6,19 +7,21 @@ const OneProduct = ({data}) => {
 
     const {bookTitle, bookAuthor, oldPrice, bookPrice, bookRating, bookImage, store} = data
 
+    const cartProduct = useContext(ProductContext)
+    const addingToCart= cartProduct.addToCart
+
     return (
-        <article className="product">
-                <header className="product-container">
-                    <figure className="gallery" id="gallery">
-                        <Image src={bookImage} alt={`{bookTitle}`} />
-                        <img src={AuthorImg} alt="author image" id="img2" className="gallery-img" width="100%" />
-                        {/* <ol className="gallery-controls" id="controls">
-                            <li><button className="gallery-button active" aria-controls="img1" aria-label="Image 1"></button></li>
-                            <li><button className="gallery-button" aria-controls="img2" aria-label="Image 2"></button></li>
-                        </ol> */}
-                    </figure>
-                </header>
-                <div className="product-info">
+        <div>
+            <div id="gallery" className="gallery">
+                <Image src={bookImage} alt={`{bookTitle}`} className="product-img" />
+                <img src={AuthorImg} alt="author image" id="img2" className="author-img" />
+                {/* <ol className="gallery-controls" id="controls">
+                    <li><button className="gallery-button active" aria-controls="img1" aria-label="Image 1"></button></li>
+                    <li><button className="gallery-button" aria-controls="img2" aria-label="Image 2"></button></li>
+                </ol> */}
+            </div>
+
+            <div className="product-info">
                 <h3>{bookTitle}</h3>
                 <h4>by {bookAuthor}</h4>
                 <data><del>${oldPrice}</del> <ins>${bookPrice}</ins></data>
@@ -54,12 +57,12 @@ const OneProduct = ({data}) => {
                     </div>
                     </fieldset>
                 </form>
-                <footer className="large-scale">
-                    <button type="button" className="add-cart"><span className="material-icons"></span>Add to Cart</button>
+                <footer className="product-footer">
+                    <button type="button" className="add-cart" onClick={addingToCart}><span className="material-icons"></span>Add to Cart</button><br></br>
                     <button className="favourites" type="button"><span className="material-icons"></span>Add to Favourites</button>
                 </footer>
-                </div>
-            </article> 
+            </div>
+        </div> 
     )
 }
 
